1c9f4c988a07e89cbf2a6904f52cb603ab0664c7
lmm <liumengmeng@e-future.com.cn>
2018-04-19 14:28:13 +0800

    add Tencent MTA

---
+++ app/src/main/java/club/biggerm/asionbo/wanandroid/MyApplication.kt
@@ -0,0 +1,25 @@
+package club.biggerm.asionbo.wanandroid
+
+import android.app.Application
+import com.tencent.tac.TACApplication
+import com.tencent.tac.analytics.TACAnalyticsOptions
+import com.tencent.tac.analytics.TACAnalyticsService
+import com.tencent.tac.analytics.TACAnalyticsStrategy
+import com.tencent.tac.option.TACApplicationOptions
+
+/**
+ * Created by asionbo on 2018/4/19.
+ */
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        TACApplication.configure(this)
+
+        val tacApplicationOptions:TACApplicationOptions = TACApplicationOptions.newDefaultOptions(this)
+        val tacAnalyticsOptions: TACAnalyticsOptions = tacApplicationOptions.sub("analytics")
+
+        tacAnalyticsOptions.strategy(TACAnalyticsStrategy.INSTANT)
+
+        TACAnalyticsService.getInstance().start(this)
+    }
+}
